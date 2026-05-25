bc04dfe353c0c4387869c38fc02c131fb7500e87
Mainuddeen00 <mainuddeen9565@gmail.com>
2026-05-26 00:08:08 +0530

    fix: replaced annotation with global CORS configuration

--- src/main/java/com/greenops/sentinel/controller/AuditController.java
+++ src/main/java/com/greenops/sentinel/controller/AuditController.java
@@ -2,11 +2,13 @@ package com.greenops.sentinel.controller;
 
 import com.greenops.sentinel.entity.AuditResult;
 import com.greenops.sentinel.repository.AuditRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/audits")
